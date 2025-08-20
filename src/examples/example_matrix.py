1f677519c780d4809e12ee9fa964c462a371a162
Paul G Nelson <pgnelson307@gmail.com>
2025-08-20 13:11:55 -0400

    Update example_matrix.py

--- src/examples/example_matrix.py
+++ src/examples/example_matrix.py
@@ -1,4 +1,6 @@
 from rankensemblesig import calc_matrix_significance
+import numpy as np
+
 def main():
   test_dat = np.array([[1,2,3, 4], [2,1,3, 4], [1,3,4,2]])
   print("Test matrix: " + str(test_dat))
