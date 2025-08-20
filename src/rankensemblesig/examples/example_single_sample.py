c8d47e9fe872968dc0cb2be5e28e7dff2528c88c
Paul G Nelson <pgnelson307@gmail.com>
2025-08-20 12:42:51 -0400

    Update example_single_sample.py

--- src/rankensemblesig/examples/example_single_sample.py
+++ src/rankensemblesig/examples/example_single_sample.py
@@ -2,4 +2,4 @@ from rankensemblesig import calc_rank_ensemble_pvalue
 def main():
   ranks = np.array([1,1,2,2])
   print("Example ranks: " + str(ranks))
-  calc_rank_ensemble_pvalue(ranks, 4)
+  print(calc_rank_ensemble_pvalue(ranks, 4))
