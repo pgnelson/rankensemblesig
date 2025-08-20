ef5bb3f81ffa865728eaeb98670767430e2e74e0
Paul G Nelson <pgnelson307@gmail.com>
2025-08-20 13:25:48 -0400

    Update example_single_sample.py

--- src/examples/example_single_sample.py
+++ src/examples/example_single_sample.py
@@ -4,4 +4,4 @@ import numpy as np
 def main():
   ranks = np.array([1,1,2,2])
   print("Example ranks: " + str(ranks))
-  print(calc_rank_ensemble_pvalue(ranks, 4))
+  print("P-value: " + np.exp(calc_rank_ensemble_pvalue(ranks, 4)))
