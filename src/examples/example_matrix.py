736ef2506692c6ee6e9a2d98c912e33297955773
Paul G Nelson <pgnelson307@gmail.com>
2025-08-20 13:15:11 -0400

    Update example_matrix.py

--- src/examples/example_matrix.py
+++ src/examples/example_matrix.py
@@ -4,4 +4,4 @@ import numpy as np
 def main():
   test_dat = np.array([[1,2,3, 4], [2,1,3, 4], [1,3,4,2]])
   print("Test matrix: " + str(test_dat))
-  print(calc_rank_ensemble_pvalue(test_dat))
+  print(calc_matrix_significance(test_dat))
